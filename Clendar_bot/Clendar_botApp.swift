bb28f68a5c45048c516a08a0842b91743c4ead0c
Rudem <alexgridserver@gmail.com>
2026-06-10 16:02:23 +0300

    Initial Commit

---
+++ Clendar_bot/Clendar_botApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clendar_botApp.swift
+//  Clendar_bot
+//
+//  Created by Design Yousupova on 10.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Clendar_botApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clendar_botTests/Clendar_botTests.swift
@@ -0,0 +1,19 @@
+//
+//  Clendar_botTests.swift
+//  Clendar_botTests
+//
+//  Created by Design Yousupova on 10.06.2026.
+//
+
+import Testing
+@testable import Clendar_bot
+
+struct Clendar_botTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
